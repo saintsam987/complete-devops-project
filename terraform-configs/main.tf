89531e31462bc32345c61663565d665df031c016
Samuel Okafor <samuelokafor987@gmail.com>
2024-12-31 17:32:10 -0800

    check

--- terraform-configs/main.tf
+++ terraform-configs/main.tf
@@ -15,7 +15,6 @@ provider "minikube" {
 resource "minikube_cluster" "minikibe_docker" {
     driver = "docker"
     cluster_name = "complete-devops-project"  
-    cpus   = "2"    
     memory = "2048"
     addons = [
         "default-storageclass",
